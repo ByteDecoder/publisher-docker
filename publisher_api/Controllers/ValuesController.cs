6754926e3997d56336a155512043b134b22ec837
ByteCoder <rreyes@canaryhealth.com>
2020-02-15 00:53:20 -0600

    Code refactored

--- publisher_api/Controllers/ValuesController.cs
+++ publisher_api/Controllers/ValuesController.cs
@@ -2,24 +2,20 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace publisher_api.Controllers
-{
+namespace publisher_api.Controllers {
   [ApiController]
   [Route("api/[controller]")]
-  public class ValuesController : ControllerBase
-  {
+  public class ValuesController: ControllerBase {
 
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
-    {
-      return new string[] { "value1", "value2" };
+    public ActionResult<IEnumerable<string>> Get() {
+      return new[] { "value1", "value2" };
     }
 
     // POST api/values
     [HttpPost]
-    public IActionResult Post(string payload)
-    {
+    public IActionResult Post(string payload) {
       return Ok(new { sucess = true });
     }
   }
--- worker/Program.cs
+++ worker/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace worker
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace worker {
+  class Program {
+    public static async Task PostMessage(string postData) {
+      var json = JsonConvert.SerializeObject(postData);
+      var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+      using var httpClientHandler = new HttpClientHandler {
+        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+      };
+      using var client = new HttpClient(httpClientHandler);
+      var result = await client.PostAsync("https://localhost:5001/api/values", content);
+      var resultContent = await result.Content.ReadAsStringAsync();
+      Console.WriteLine($"Server returned: {resultContent}");
     }
+
+    static void Main() {
+      Console.WriteLine("Hello World!");
+    }
+  }
 }
