ee842592cec5b8cbafa1419468e88b8b7bdd7283
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-02-18 12:19:27 -0600

    Fixing References

--- UtilsLibrary/Builder.cs
+++ UtilsLibrary/Builder.cs
@@ -1,18 +1,23 @@
-public class Builder <T> where T: new ( ) 
+using System;
+using System.Collections.Generic;
+
+public class Builder<T> where T : new()
 {
-  IList <Action <T>> actions = new List <Action <T>> ( ) ;
-  public T Build ( ) 
+  IList<Action<T>> actions = new List<Action<T>>();
+
+  public T Build()
   {
-    var built = new T ( ) ;  
-    foreach ( var action in actions ) 
+    var built = new T();
+    foreach (var action in actions)
     {
-      action ( built ) ; 
+      action(built);
     }
     return built;
   }
-  public Builder <T> With ( Action <T> with )
+
+  public Builder<T> With(Action<T> with)
   {
-    actions. Add ( with ) ; 
-    return this ; 
+    actions.Add(with);
+    return this;
   }
 }
