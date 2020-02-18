56be7035e58e2be782865457c7cb2a01d7b9690a
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-02-17 18:10:30 -0600

    Create Builder.cs

---
+++ UtilsLibrary/Builder.cs
@@ -0,0 +1,18 @@
+public class Builder <T> where T: new ( ) 
+{
+  IList <Action <T>> actions = new List <Action <T>> ( ) ;
+  public T Build ( ) 
+  {
+    var built = new T ( ) ;  
+    foreach ( var action in actions ) 
+    {
+      action ( built ) ; 
+    }
+    return built;
+  }
+  public Builder <T> With ( Action <T> with )
+  {
+    actions. Add ( with ) ; 
+    return this ; 
+  }
+}
